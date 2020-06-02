38d518120a51af6884ae771bd00a10bd0f88cd59
karinmeijvogel <karinmeijvogel@hotmail.com>
2020-06-02 22:55:47 +0200

    :sparkles: Markers drawn on map, based on long-lat locations!

--- static/scripts/index.js
+++ static/scripts/index.js
@@ -1,6 +1,9 @@
-const svg = d3.select('svg')
-    .style('width', '960px')
-    .style('height', '500px');
+const svg = d3.select("svg")
+    .style("width", "960px")
+    .style("height", "500px");
+
+let borders = svg.selectAll("path")
+let marks = svg.selectAll(".mark")
 
 const projection = d3.geoMercator();
 const pathGenerator = d3.geoPath().projection(projection);
@@ -10,10 +13,20 @@ d3.json("https://cdn.jsdelivr.net/npm/world-atlas@2/countries-110m.json")
 
 function ready(data) {
     const countries = topojson.feature(data, data.objects.countries);
+    const locations = [{long: 5, lat: 52},{long: -78, lat: 41},{long: -70, lat: 53}];
 
-    svg.selectAll('path')
+    borders
         .data(countries.features)
         .enter()
-        .append('path')
-        .attr('d', pathGenerator);
+        .append("path")
+        .attr("d", pathGenerator);
+
+    marks
+        .data(locations)
+        .enter()
+        .append("circle")
+        .attr("class", "mark")
+        .attr("r", 3)
+        .attr("fill", "red")
+        .attr("transform", d => `translate(${projection([d.long,d.lat])})`);
 }
